8b6c77250640079c7263a11d00466c82ed6950ba
kumavis <aaron@kumavis.me>
2019-04-18 20:17:50 +0800

    Updates

---
+++ precache-manifest.2318c61cb9ba41ce889603ff78c6edc2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe76d4f1489dcdd5726f",
+    "url": "/metamask-deps-explorer/static/js/runtime~main.db9cb8e6.js"
+  },
+  {
+    "revision": "eec0dde1767d70f4d1ac",
+    "url": "/metamask-deps-explorer/static/js/main.885fdc27.chunk.js"
+  },
+  {
+    "revision": "06e00ccbea0585bafedb",
+    "url": "/metamask-deps-explorer/static/js/2.fe5a77b2.chunk.js"
+  },
+  {
+    "revision": "eec0dde1767d70f4d1ac",
+    "url": "/metamask-deps-explorer/static/css/main.a0683dd3.chunk.css"
+  },
+  {
+    "revision": "d42e3a71d1b1789602f361bf16acb055",
+    "url": "/metamask-deps-explorer/index.html"
+  }
+];
